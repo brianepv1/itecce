e40e34830749e449d92bcd590b70c9d78057b33b
Brian Palomar <brian.epv1@gmail.com>
2024-10-08 17:40:41 -0600

    [FEATURE]: Update about is page with styles, images and content

---
+++ src/constants/constants.js
@@ -0,0 +1,6 @@
+export const GENERAL = {
+	title: 'Itecce',
+	slogan: 'Itecce: Innovación y Excelencia en Educación',
+	marketing: 'Crece y aprende con nuestros programas presenciales y virtuales, conoce nuestras ventajas.',
+	buttonProgram: 'Conoce nuestros programas'
+}
---
+++ src/constants/values.js
@@ -0,0 +1,50 @@
+export const values = [
+  {
+    icon: "images/Valores/ValoresEstudiantes.svg",
+    title: "Primero el alumno",
+    description: "En Itecce reforzamos nuestros valores tomando como base el alumno, buscando siempre la excelencia en nuestros servicios.",
+    altText: "Reading icon",
+    nodeId: "w-node-_688cdd22-e08b-5ed1-155d-d066ce5d0021-2082f597",
+    dataWId: "688cdd22-e08b-5ed1-155d-d066ce5d0021"
+  },
+  {
+    icon: "images/Valores/ValoresExcelencia.svg",
+    title: "Excelencia Académica",
+    description: "Compromiso con ofrecer una educación de alta calidad, fomentando el desarrollo integral de los estudiantes a través de planes de estudios actualizados y docentes altamente capacitados.",
+    altText: "Mobile with a fingerprint icon on it",
+    nodeId: "w-node-fb068d3b-fe63-d731-5de0-639e0d4eaf51-2082f597",
+    dataWId: "fb068d3b-fe63-d731-5de0-639e0d4eaf51"
+  },
+  {
+    icon: "images/Valores/ValoresInclusion.svg",
+    title: "Inclusión y Diversidad",
+    description: "Valorar y respetar la diversidad cultural, de pensamiento y de aprendizaje de los estudiantes, creando un ambiente inclusivo donde todos puedan desarrollarse plenamente.",
+    altText: "Icon",
+    nodeId: "w-node-_45e4da8d-b204-306a-7aae-04747eeebbe8-2082f597",
+    dataWId: "45e4da8d-b204-306a-7aae-04747eeebbe8"
+  },
+  {
+    icon: "images/Valores/ValoresResponsabilidad.svg",
+    title: "Responsabilidad Social",
+    description: "Promover el compromiso con la comunidad y el entorno, inculcando en los estudiantes un sentido de responsabilidad hacia la sociedad y el medio ambiente.",
+    altText: "a green and black circle with circles around it",
+    nodeId: "w-node-b895dc0a-e423-bafd-bda6-f8f67d95d0bb-2082f597",
+    dataWId: "b895dc0a-e423-bafd-bda6-f8f67d95d0bb"
+  },
+  {
+    icon: "images/Valores/ValoresFormacion.svg",
+    title: "Formación Ética y Profesional",
+    description: "Fomentar principios éticos y morales en la formación de profesionales que actúen con integridad y responsabilidad en sus campos de trabajo.",
+    altText: "a green circle with a lightning bolt inside of it",
+    nodeId: "w-node-_1d7868d2-7399-d18c-7f11-96361c0d199e-2082f597",
+    dataWId: "1d7868d2-7399-d18c-7f11-96361c0d199e"
+  },
+  {
+    icon: "images/Valores/ValoresInnovacion.svg",
+    title: "Innovación Educativa",
+    description: "Promover la creatividad y el uso de tecnologías emergentes en los procesos de enseñanza y aprendizaje para mejorar la experiencia educativa y mantenerse a la vanguardia.",
+    altText: "a gear wheel with a leaf inside of it",
+    nodeId: "w-node-eae2b481-d650-fa97-2632-ac067ffa98d6-2082f597",
+    dataWId: "eae2b481-d650-fa97-2632-ac067ffa98d6"
+  }
+];
