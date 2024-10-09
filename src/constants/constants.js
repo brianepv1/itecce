36132dd2f14de3a3e79339abed428819581d3e42
Brian Palomar <brian.epv1@gmail.com>
2024-10-08 23:57:28 -0600

    [Fix]: fix og

--- src/constants/constants.js
+++ src/constants/constants.js
@@ -1,5 +1,5 @@
 export const GENERAL = {
-	title: 'Itecce - Conoce nuestra historia, somos itecce',
+	title: 'Itecce - Innovación y Excelencia en Educación',
 	description: 'Descubre la Universidad que transforma vidas: donde tu futuro comienza hoy, impulsado por la innovación, el conocimiento y el liderazgo global.',
 	slogan: 'Itecce: Innovación y Excelencia en Educación',
 	marketing: 'Crece y aprende con nuestros programas presenciales y virtuales, conoce nuestras ventajas.',
